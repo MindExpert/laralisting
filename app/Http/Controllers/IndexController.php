3601c87a6a8b1ba83fb1b9098ba4317ac6cf6b5e
e.muchasaj <e.muchasaj@7btc.co>
2023-11-23 09:56:45 +0100

    fixes

---
+++ app/Http/Controllers/IndexController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class IndexController extends Controller
+{
+    public function index()
+    {
+        return inertia('Index/Index', [
+            'message' => 'Hello from Laravel!'
+        ]);
+    }
+
+    public function show()
+    {
+        return inertia('Index/Show');
+    }
+}
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,23 @@
-import './bootstrap';
+import { createApp, h } from 'vue'
+// import { createInertiaApp, Link, Head } from '@inertiajs/inertia-vue3';
+import { createInertiaApp, Link, Head } from '@inertiajs/vue3'
+import MainLayout from "./Layouts/MainLayout.vue";
+
+createInertiaApp({
+    resolve: async (name) => {
+        const pages = import.meta.glob('./Pages/**/*.vue')
+        const page = await pages[`./Pages/${name}.vue`]();
+
+        // Set a Default Layout, when not defined explicitly in the Page or 'undefined'
+        page.default.layout = page.default.layout || MainLayout;
+
+        return page;
+    },
+    setup({el, App, props, plugin}) {
+        createApp({render: () => h(App, props)})
+            .use(plugin)
+            .component("Head", Head)
+            .component("Link", Link)
+            .mount(el);
+    },
+});
